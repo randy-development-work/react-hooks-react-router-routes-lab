bf7e44bd9cc2d59f9a1e0512758304df03ada77f
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-19 00:27:39 +0300

    Render Links in NavBar and App.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <ul>
+      <li><NavLink 
+        to="/"
+        exact
+      >
+        Home
+      </NavLink> </li>
+      <li><NavLink 
+        to="/movies"
+        exact
+      >
+        Movies
+      </NavLink></li>
+      <li><NavLink 
+        to="/directors"
+        exact
+      >
+        Directors
+      </NavLink></li>
+      <li><NavLink 
+        to="/actors"
+        exact
+      >
+        Actors
+      </NavLink></li>
+      </ul>
+    </div>
+  
+  );
 }
 
 export default NavBar;
