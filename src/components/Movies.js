117e0c95ffe4262a6338fce8a097e8792f4dff4c
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-19 11:18:11 +0300

    Render movies in Movies Page.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { movies } from "../data";
 
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieList = movies.map((movie) => (
+    <div key={movie.title}>
+      <h1>Name: {movie.title}</h1>
+      <p>Time: {movie.time}</p>
+      <p>Genres: </p>
+      <ul>
+        Genres:
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}  
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+      
+    </div>);
 }
 
 export default Movies;
